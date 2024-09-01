9c56d0907c244aa1079b55f644e92ac40e7929b6
Himanshu <himanshuingle42.com>
2024-09-01 08:55:02 +0530

    Remove all files from Git tracking

--- Frontend/src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export const forceDownload = (url, filename = "") => {
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = filename; // Specify filename if needed
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
